68dbc2f7263b11e6f50922f9d9ff636cf336436b
Denis Sergienko <olol.toor@gmail.com>
2014-05-12 16:51:13 +0300

    generator done

--- generator.sh
+++ generator.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-hostname=$1
+# colors
+RED_BACK='\033[0;41;30m'
+STD_BACK='\033[0;0;39m'
+
+GRN_FONT='\e[1;32m'
+STD_FONT='\e[0m'
+
+# host config file render functions
+
+function generate_host {
+  hostname=$1
+  echo -e "${GRN_FONT}Creating host config for $hostname${STD_FONT}"
+  ask_attribute 'public_folder' # get value and create "public_folder"
+
+  config_file="$NGINX_PATH/sites-avalilable/$hostname.conf"
+  mkdir -p "$NGINX_PATH/sites-avalilable/"
+
+  echo '' > "$config_file" # clear previous file
+
+  render "templates/default.tmpl.conf"
+
+  echo "Config file created!"
+  exit
+}
 
 function ask_attribute {
   echo "Set value of $1:"
@@ -21,14 +44,35 @@ function render {
 }
 
 
-echo "Creating config for $hostname"
+# install config files functions
 
-ask_attribute 'public_folder' # get value and create "public_folder"
+function install_configs {
+  cp -r conf "$NGINX_PATH"
+  cp nginx.conf "$NGINX_PATH"
+  echo -e "${GRN_FONT}Config files copied into $NGINX_PATH ${STD_FONT}"
+  exit
+}
 
-config_file='generated/default.conf'
-mkdir -p 'generated'
-echo '' > "$config_file" # clear previous file
 
-render "templates/default.tmpl.conf"
+# script section
 
-echo "Config file created!"
+if ! [ -n "$NGINX_PATH" ]
+  then
+    echo -e "${RED_BACK}NGINX_PATH is not set${STD_BACK}"
+    echo "default is NGINX_PATH='/etc/nginx'"
+
+    NGINX_PATH='/etc/nginx'
+  fi
+
+if [ "$1" == 'host' ]
+  then
+    generate_host $2
+  fi
+
+if [ "$1" == 'install' ]
+  then
+    install_configs
+  fi
+
+
+echo -e "${GRN_FONT}NOTHING TO DO${STD_FONT}"
